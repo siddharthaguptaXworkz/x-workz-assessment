7e107653d0947576c831a42c19b568296f5650c1
Siddhartha Gupta <siddhero97@gmail.com>
2020-11-13 16:49:33 +0530

    More programs

--- program-files/Animal.java
+++ program-files/Animal.java
@@ -43,8 +43,22 @@ public class Animal{
 	}
 	
 	public void printAnimal(boolean includesIsMammal) {
-		
+		if(includesIsMammal) {
+			System.out.println("animal with name "+ animal.getName() + "is mammal " + animal.getIsMammal() + "of species "+ animal.getSpecies() + "weight "+animal.getWeight() );
+		} else {
+			System.out.println("animal with name "+ animal.getName() + "of species "+ animal.getSpecies()+ "weight "+animal.getWeight()  );
+		}
 	}
 	
-	public void printAnimal(boolean includesIsMamml, boolean includesWeight)
+	public void printAnimal(boolean includesIsMammal, boolean includesWeight) {
+		if(includesIsMammal && includesWeight) {
+			System.out.println("animal with name "+ animal.getName() + "is mammal " + animal.getIsMammal() + "of species "+ animal.getSpecies()+ "weight "+animal.getWeight()  );
+		} else if (!includesIsMammal && includesWeight) {
+			System.out.println("animal with name "+ animal.getName() + "of species "+ animal.getSpecies()+ "weight "+animal.getWeight()  );
+		} else if(!includesIsWeight && includesIsMammal) {
+			System.out.println("animal with name "+ animal.getName() + "is mammal " + animal.getIsMammal() + "of species "+ animal.getSpecies() );
+		} else {
+			System.out.println("animal with name "+ animal.getName() + "is mammal " + animal.getIsMammal() + "of species "+ animal.getSpecies()+ "weight "+animal.getWeight()  );
+		}
+	}
 }
---
+++ program-files/Array.java
@@ -0,0 +1,10 @@
+public class Array {
+	public static void main(String[] args) {
+		int numbers[5] = {0,1,2,3,4};
+		for (int i =0; i< numbers.length;i++) {
+			
+			System.out.println("index "+i+" value "+numbers[i]);
+		}
+		
+	}
+}
